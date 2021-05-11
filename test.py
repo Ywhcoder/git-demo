717ad83cf0a893a54069453bb289612b8ab90b2d
yangweihan <yangweihan0522@163.com>
2021-05-11 21:05:09 +0800

    第二次提交

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 '''
 Author: YangWeiHan
 Date: 2021-05-09 21:59:30
-LastEditTime: 2021-05-10 21:15:00
+LastEditTime: 2021-05-11 21:04:44
 FilePath: /TestDemo/test.py
 '''
 arr1 = [1,2,3,4]
 arr2 = [5,6,7,8]
 arr3 = zip(arr1, arr2)
-type(sum(arr3))
